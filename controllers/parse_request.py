038909a004da9b160dd3035e626defa78f9e0395
Maksym Andreiev <maxoggyha@gmail.com>
2021-09-03 23:36:44 +0300

    init

---
+++ controllers/parse_request.py
@@ -0,0 +1,12 @@
+from flask import request
+
+
+def get_request_data():
+    """
+    Get keys & values from request (Note that this method should parse requests with content type "application/x-www-form-urlencoded")
+    """
+    if request.content_type.startswith('application/x-www-form-urlencoded'):
+        data = request.data
+    else:
+        data = None
+    return data
