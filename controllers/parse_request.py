21fd76e01712387f1c4f9ce45b9acdebcb3e6b74
Maksym Andreiev <maxoggyha@gmail.com>
2024-04-14 16:31:46 +0900

    Add check if request is empty

--- controllers/parse_request.py
+++ controllers/parse_request.py
@@ -5,8 +5,11 @@ def get_request_data():
     """
     Get keys & values from request (Note that this method should parse requests with content type "application/x-www-form-urlencoded")
     """
-    if request.content_type.startswith('application/x-www-form-urlencoded'):
-        data = request.form.to_dict()
+    if request.content_type:
+        if request.content_type.startswith('application/x-www-form-urlencoded'):
+            data = request.form.to_dict()
+        else:
+            data = {}
+        return data
     else:
-        data = {}
-    return data
+        return {}
